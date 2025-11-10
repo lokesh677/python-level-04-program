3d3058f53bd66316cb63b718e798f25d5589bd64
lokesh677 <lokeshthala142@gmail.com>
2025-11-10 10:50:55 +0530

    Add files via upload

---
+++ lokesh17.py
@@ -0,0 +1,11 @@
+print("print sum of n number of value")
+print("------------------------------")
+n=int(input("Enter the number:"))
+print("**result**")
+sum=0
+for i in range (1,n+1):
+    print("",+i)
+    sum=sum+i
+print("sum of total number:")
+print(sum)
+                       
